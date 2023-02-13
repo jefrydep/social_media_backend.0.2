6e192ef7ee8fc5f397625b1794cf3816664b6bc2
jefry <jefrydep@gmail.com>
2023-02-13 07:02:38 -0500

    last commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv').config()
 const cors = require('cors')
 const authController = require('./controllers/authController')
+const userController = require('./controllers/userController')
+const postController = require('./controllers/postController')
+const commentController = require('./controllers/commentController')
 
 // const authController = require('../src/controllers/authController')
 const app = express()
@@ -13,7 +16,9 @@ mongoose.connect(process.env.MONGO_URL,()=> console.log( 'db is connected'));
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use('/auth',authController)
-
+app.use('/user',userController)
+app.use('/post',postController)
+app.use('/comment',commentController)
 
 
 
